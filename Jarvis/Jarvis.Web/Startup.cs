dfdf588c206594fadbb2e3991f33dbf549c9de07
David <dmdinh22@gmail.com>
2017-06-13 00:17:23 -0700

    project scaffolding

---
+++ Jarvis/Jarvis.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jarvis.Web.Startup))]
+namespace Jarvis.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
