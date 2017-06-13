fba4075ca16f5861b4ffa84ecc4f4aaad04504a1
David <dmdinh22@gmail.com>
2017-06-13 15:34:37 -0700

    Theme implemented, started Task feature

--- Jarvis/Jarvis.Web/App_Start/BundleConfig.cs
+++ Jarvis/Jarvis.Web/App_Start/BundleConfig.cs
@@ -14,17 +14,27 @@ namespace Jarvis.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
+                        "~/Scripts/angular.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/tether").Include(
+                        "~/Scripts/tether.js",
+                        "~/Scripts/tether.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                      "~/Scripts//jarvis/app.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/css/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
