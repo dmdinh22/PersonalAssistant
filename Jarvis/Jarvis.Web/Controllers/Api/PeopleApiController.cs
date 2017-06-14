aaf819109791e96c37fa43e5673829bef0f35356
David <dmdinh22@gmail.com>
2017-06-13 23:41:07 -0700

    read from db for contacts page implemented

---
+++ Jarvis/Jarvis.Web/Controllers/Api/PeopleApiController.cs
@@ -0,0 +1,23 @@
+﻿using Jarvis.Web.Models.ViewModels;
+using Jarvis.Web.Services;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Jarvis.Web.Controllers.Api
+{
+    [RoutePrefix("api/people")]
+    public class PeopleApiController : ApiController
+    {
+        [Route]
+        [HttpGet]
+        public HttpResponseMessage GetPeople()
+        {
+            PeopleService peopleSvc = new PeopleService();
+            List<Person> peopleList = peopleSvc.GetPeople();
+            return Request.CreateResponse(HttpStatusCode.OK, peopleList);
+        }
+    }
+}
---
+++ Jarvis/Jarvis.Web/Services/PeopleService.cs
@@ -0,0 +1,44 @@
+﻿using Jarvis.Web.Models.ViewModels;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Jarvis.Web.Services
+{
+    public class PeopleService
+    {
+        public List<Person> GetPeople()
+        {
+            List<Person> peopleList = new List<Person>();
+
+            // setting connection string to a variable
+            string connString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            
+            //establish connection
+            using (SqlConnection sqlConn = new SqlConnection(connString))
+            {
+                // establlish command object
+                using (SqlCommand cmd = new SqlCommand("dbo.People_GetAll", sqlConn))
+                {
+                    sqlConn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                    while (reader.Read())
+                    {
+                        Person p = new Person();
+                        int startingIndex = 0;
+
+                        //reading data from db
+                        p.Id = reader.GetInt32(startingIndex++);
+                        p.FirstName = reader.GetString(startingIndex++);
+                        p.LastName = reader.GetString(startingIndex++);
+                        p.PhoneNumber = reader.GetString(startingIndex++);
+                        p.Email = reader.GetString(startingIndex++);
+
+                        peopleList.Add(p);
+                    }
+
+                }
+            }
+            return peopleList;
+        }
+    }
+}
